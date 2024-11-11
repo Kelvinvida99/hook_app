40fc4c69a5bf46f7941439299c41245279615ec6
Kelvinvida99 <kelvinvida99@gmail.com>
2024-11-10 22:43:30 -0400

    change

--- src/components/useFetch/MultiplesCustomHook.jsx
+++ src/components/useFetch/MultiplesCustomHook.jsx
@@ -5,7 +5,7 @@ export const MultiplesCustomHook = () => {
 
   return (
     <div>
-      <p>Informacion de Pokemon</p>
+      <p>Informacion de Gonfig</p>
 
       {isLoading && <h4>Cargando...</h4>}
 
